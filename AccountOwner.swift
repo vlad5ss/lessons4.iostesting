fa2ba75b2dfecc8be571388398d8acc4a51737bb
vlad5ss <vlad5grup@gmail.com>
2017-07-26 11:45:28 +0400

    Added account owner

---
+++ AccountOwner.swift
@@ -0,0 +1,15 @@
+//
+//  AccountOwner.swift
+//  lessons4.iostesting
+//
+//  Created by mac on 7/24/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AccountOwner: NSObject {
+    init?(firstName:String, lastName:String, emailAddress:String) {
+        super.init()
+    } }
